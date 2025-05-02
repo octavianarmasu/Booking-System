56e89f714bb7022da7b92d5d086faabb1deb039b
Danut1445 <dan_andrei.tunsoiu@stud.acs.upb.ro>
2025-05-02 10:35:19 +0300

    Added Users database

--- main/database.py
+++ main/database.py
@@ -3,6 +3,8 @@ import sqlite3
 def add_rezervare(id_camera, id_hotel, data_cazare, data_eliberare):
     conn = sqlite3.connect('hotel_database.db')
     cursor = conn.execute('''SELECT MAX(id) FROM Rezervari;''')
+    for row in cursor:
+        max = row[0]
     conn.execute('''PRAGMA foreign_keys = ON;''')
 
     command = f'INSERT INTO Rezervari VALUES({max + 1}, {data_cazare}, {data_eliberare}, {id_hotel}, {id_camera});'
@@ -12,6 +14,10 @@ def add_rezervare(id_camera, id_hotel, data_cazare, data_eliberare):
     except sqlite3.Error as error:
         print('Error occurred - ', error)
         return -1
+
+    # cursor = conn.execute('''SELECT * FROM Rezervari''')
+    # for row in cursor:
+    #     print(str(row[0]) + " " + str(row[1]) + " " + str(row[2]) + " " + str(row[3]))
 
     cursor.close()
     conn.execute('COMMIT')
@@ -92,4 +98,37 @@ def chech_rezervare(id):
     conn.close()
     return tabel
 
-print(chech_rezervare(0))
+def add_user(email, first_name, last_name, phone, password):
+    conn = sqlite3.connect('hotel_database.db')
+    command = f'''INSERT INTO Users VALUES('{email}', '{first_name}', '{last_name}', '{phone}', '{password}');'''
+
+    try:
+        conn.execute(command)
+    except sqlite3.Error as error:
+        print('Error occurred - ', error)
+        return -1
+
+    conn.execute('COMMIT')
+    conn.close()
+    return 0
+
+def login_check_info(email, password):
+    conn = sqlite3.connect('hotel_database.db')
+    cursor = conn.execute('SELECT * FROM Users;')
+
+    tabel = []
+    for row in cursor:
+        tabel.append({'Email': row[0], 'First Name': row[1], 'Last Name': row[2], 'Phone': row[3], 'Password': row[4]})
+    # print(tabel)
+
+    if len(tabel) < 1:
+        return False
+
+    if tabel[0]['Password'] == password:
+        return True
+    else:
+        return False
+
+add_user('example@email.com', 'User', 'Example', '000000001', 'example_password')
+print(login_check_info('example@email.com', 'example_password'))
+
